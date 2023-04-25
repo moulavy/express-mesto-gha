6d3e91bfcff9a6f5de5a50c9f78ac4457a2f7f8d
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-04-25 13:42:12 +0300

    not found page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const {NOT_FOUND_CODE}=require('./utils/constans')
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -22,5 +23,9 @@ app.use((req, res, next) => {
 
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
+
+app.use('*',(req, res) => {
+  res.status(NOT_FOUND_CODE).send({ message: 'Cтраницы не существует' });
+});
 
 app.listen(PORT);

cd900eb79b6f67f92b927e52c57e57a3df0beabf
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-04-25 13:44:17 +0300

    eslint

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const {NOT_FOUND_CODE}=require('./utils/constans')
+const { NOT_FOUND_CODE } = require('./utils/constans');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -24,7 +24,7 @@ app.use((req, res, next) => {
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 
-app.use('*',(req, res) => {
+app.use('*', (req, res) => {
   res.status(NOT_FOUND_CODE).send({ message: 'Cтраницы не существует' });
 });
 

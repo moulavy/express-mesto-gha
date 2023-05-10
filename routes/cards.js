b3f7c7f3e983f18144b0ce87ba8178079ebd145b
Milana Kulieva <72352010+moulavy@users.noreply.github.com>
2023-05-10 13:50:00 +0300

    joi in cards

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
 
 const {
   createCard, getCards, deleteCard, likeCard, dislikeCard,
@@ -13,10 +14,18 @@ router.post('/', celebrate({
 }),createCard);
 router.delete('/:cardId', celebrate({
   params: Joi.object().keys({
-    cardId:Joi.string().
-  })
+    cardId: Joi.string().alphanum().hex().length(24),
+  }),
 }),deleteCard);
-router.put('/:cardId/likes', likeCard);
-router.delete('/:cardId/likes', dislikeCard);
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().hex().length(24),
+  }),
+}), likeCard);
+router.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().alphanum().hex().length(24),
+  }),
+}), dislikeCard);
 
 module.exports = router;
